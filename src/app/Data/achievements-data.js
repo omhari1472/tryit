2b96d1df4ebb71d503664bee5a0c8ba5161a5e01
devaanshi15 <devaanshic1509@gmail.com>
2024-08-01 14:43:15 +0530

    achievements

--- src/app/Data/achievements-data.js
+++ src/app/Data/achievements-data.js
@@ -1,17 +1,25 @@
 const achieve = [
 	{
-		achievement: "Participated in Hack4Bengal hackathon, 2023",
+		achievement: "Bobble AI track WE-Hack'24 winner",
 		thumbnail: "/achievements/h4b.webp"
+		//website: "https://adagodord.com",
 	},
 	{
-		achievement: "5 ⭐ in C++ and Java on HackerRank",
+		achievement: "Mayukh Techfest winner",
 		thumbnail: "/achievements/hackerrank.webp"
 	},
 	{
-		achievement: "Completed 3 milestones in Google Cloud Facilitator Programme, 2022",
+		achievement: "Jagriti AI Hackathon, IIT BHU - team in top 10 finalists",
 		thumbnail: "/achievements/gcloud.webp"
-
-	}
+	},
+	{
+		achievement: "AIR 13 in HackQuest (by Coding Ninjas)",
+		thumbnail: "/achievements/gcloud.webp"
+	},
+	{
+		achievement: "Cleared Ideation round of Myntra HackerRamp among 29,000 entries",
+		thumbnail: "/achievements/gcloud.webp"
+	},
 ];
 
 export default achieve;
